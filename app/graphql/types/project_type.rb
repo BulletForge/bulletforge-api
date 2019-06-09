38d56b493b02739fc2f7b69d2b953b4687abb894
Abner Qian <LargeBagel@gmail.com>
2019-06-09 16:34:48 -0700

    Basic working graphql queries for users and projects

--- app/graphql/types/project_type.rb
+++ app/graphql/types/project_type.rb
@@ -1,4 +1,8 @@
 module Types
   class ProjectType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: true
+    field :description, String, null: true
+    field :version, String, null: true
   end
 end
--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,4 +1,14 @@
 module Types
   class UserType < Types::BaseObject
+    field :id, ID, null: false
+    field :permalink, String, null: true
+    field :email, String, null: true
+    field :login, String, null: true
+    field :projects, [Types::ProjectType], null: true
+    field :projects_count, Integer, null: true
+
+    def projects_count
+      projects.size
+    end
   end
 end
