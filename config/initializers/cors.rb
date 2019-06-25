733d3621a067c29e7b2c9143ab4a40272e71493c
Abner Qian <LargeBagel@gmail.com>
2019-06-25 04:26:44 -0700

    Adding cors config for development env

---
+++ config/initializers/cors.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+if Rails.env.development?
+  Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+      origins 'localhost:3001'
+      resource '*',
+               headers: :any,
+               methods: %i[get post put patch delete options head]
+    end
+  end
+end
